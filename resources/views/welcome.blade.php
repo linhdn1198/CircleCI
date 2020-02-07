cccb4bd3b7cf5a487cb14e372e5a9c9d28aacd38
Duong Ngoc Linh <duong.ngoc.linh@sun-asterisk.com>
2020-02-07 09:07:00 +0700

    feat(Backend): Notify to chatwork and auto deploy

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Welcome auto deploy with Laravel
                 </div>
 
                 <div class="links">
